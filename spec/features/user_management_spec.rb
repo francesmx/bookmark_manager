1371949363cd67227d5ce6e6aea11e3d15b6c20d
crispinandrews <crispin.andrews@gmail.com>
2016-10-20 17:21:09 +0100

    Stage 22 done. Preventing duplicate accounts

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -14,13 +14,18 @@ feature 'User sign up' do
   scenario 'with a password that does not match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
   scenario "I can't sign up without an email address" do
     expect { sign_up(email: nil) }.not_to change(User, :count)
   end
   scenario "I can't sign up with an invalid email address" do
     expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
+  end
+  scenario "I can not sign up with an email that exists" do
+    sign_up
+    expect{ sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 
   def sign_up(email: 'alice@example.com',
