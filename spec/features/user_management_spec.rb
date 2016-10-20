987c63bbeab372e789304bcfcda6b82dcc211a43
Frances Maxwell <fmaxwell@gmail.com>
2016-10-20 17:11:00 +0100

    Chapter 21. Email validation.

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,5 @@
 feature 'User sign up' do
+
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
@@ -10,19 +11,25 @@ feature 'User sign up' do
     # However, it's convenient for our purposes.
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
   end
-    scenario 'with a password that does not match' do
-      expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
-      expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
-      expect(page).to have_content 'Password and confirmation password do not match'
+  scenario 'with a password that does not match' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
+    expect(page).to have_content 'Password and confirmation password do not match'
+  end
+  scenario "I can't sign up without an email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+  scenario "I can't sign up with an invalid email address" do
+    expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
   end
 
   def sign_up(email: 'alice@example.com',
             password: '12345678',
             password_confirmation: '12345678')
-  visit '/users/new'
-  fill_in :email, with: email
-  fill_in :password, with: password
-  fill_in :password_confirmation, with: password_confirmation
-  click_button 'Sign up'
-end
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
 end
